f940edb58b6a2b6369b181095073c0489ded9223
rahmanwolied <84658950+rahmanwolied@users.noreply.github.com>
2023-07-09 11:05:49 +0600

    fixed 24 hour time format glitch

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const getSchedule = fetch('schedule.json')
-	.then((response) => response.json())
-	.then((data) => {
-		return data;
-	})
-
-	.catch((error) => {
-		// Handle error if the file could not be loaded or parsed
-		console.error(error);
-	});
+const getSchedule = async ()=>{
+	try {
+		let data = await fetch('schedule.json')
+		data = await data.json()
+		// console.log(data)
+		return data
+	} catch (error) {
+		console.log("error occured")
+	}
+}
 
 async function update() {
-	const data = await getSchedule;
+	const data = await getSchedule();
+	console.log(data)
 	const schedule = data.schedule;
 	const labs = data.labs;
 
@@ -18,21 +19,28 @@ async function update() {
 	console.log('update');
 	const currentDate = new Date();
 	const currentDay = currentDate.toLocaleString('en-US', { weekday: 'short' }).slice(0, 2);
-	const currentTime = currentDate.toLocaleString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
-	console.log(currentTime);
+	let currentTime = currentDate.toLocaleString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false });
 	console.log(currentDay);
+	console.log(currentTime)
 	const arr = [];
+	currentTime = "11:12 AM"
+	console.log(currentTime)
+	console.log(typeof currentTime);
 
-	schedule.forEach((course) => {
+	schedule.forEach((course, index) => {
 		const days = course.days;
-		const matchingDay = days.find((day) => day.day === currentDay && day.start <= currentTime && day.end >= currentTime && day.type === 'Lab');
+
+		
+		const matchingDay = days.find((day) => day.day === currentDay && convertTo24HourFormat(day.start) <= currentTime && convertTo24HourFormat(day.end) >= currentTime && day.type === 'Lab');
 		if (matchingDay) {
+			console.log(matchingDay)
 			arr.push(matchingDay.room);
 		}
 	});
+	console.log(arr)
 	const occupiedLabs = [...new Set(arr)];
+	console.log(occupiedLabs)
 	const freeLabs = labs.filter((lab) => !occupiedLabs.includes(lab));
-	// const freeLabs = ['wolied', 'likes', 'sex'];
 	const container = document.querySelector('.container');
 	container.innerHTML = '';
 	container.innerHTML = `Found ${freeLabs.length} free labs`;
@@ -42,3 +50,31 @@ async function update() {
 		i++;
 	});
 }
+
+function convertTo24HourFormat(time12Hour) {
+	// Parse the time string into hours and minutes
+	const timeParts = time12Hour.split(":");
+	const hours = parseInt(timeParts[0]);
+	const minutes = parseInt(timeParts[1].split(" ")[0]);
+  
+	// Determine if it's AM or PM
+	const isPM = timeParts[1].includes("PM");
+  
+	// Convert to 24-hour format
+	let hours24 = hours;
+	if (isPM && hours !== 12) {
+	  hours24 += 12;
+	} else if (!isPM && hours === 12) {
+	  hours24 = 0;
+	}
+  
+	// Format the time as a string in 24-hour format
+	const time24Hour = hours24.toString().padStart(2, "0") + ":" + minutes.toString().padStart(2, "0");
+	return time24Hour;
+  }
+  
+  // Example usage
+  const time12Hour = "2:00 PM";
+  const time24Hour = convertTo24HourFormat(time12Hour);
+  console.log(time24Hour); // Output: "14:00"
+  
